daeb7346337467235561800dc4afb29f2732886b
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-10-25 18:18:19 +0530

    Highlighting the current active item in navbar

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,9 +1,19 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 
 @Component({
     selector: 'navbar',
     templateUrl: 'app/navbar.component.html',
     directives: [ROUTER_DIRECTIVES] 
 })
-export class NavBarComponent { }
+export class NavBarComponent { 
+    constructor(private _router: Router){
+
+    }
+
+    isCurrentRoute(route){
+        var statement = this._router.generate(route);
+        console.log(statement);
+        return this._router.isRouteActive(statement);
+    }
+}
