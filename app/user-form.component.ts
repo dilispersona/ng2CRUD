7c9731844017a98168876d772060098b38255001
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-10-27 12:23:17 +0530

    create a user , sending a post request

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,17 +1,21 @@
 import { Component } from 'angular2/core';
 import { FormBuilder, ControlGroup, Validators } from 'angular2/common';
 
-import {CanDeactivate} from 'angular2/router';
+import {CanDeactivate, Router} from 'angular2/router';
 import { BasicValidators } from './basicValidators';
+import {UserService} from './users.service';
 
 @Component({
-    templateUrl: 'app/user-form.component.html'
+    templateUrl: 'app/user-form.component.html',
+    providers: [UserService]
 })
 
 export class UserFormComponent implements CanDeactivate {
     form: ControlGroup;
 
-    constructor(fb: FormBuilder) {
+    constructor(fb: FormBuilder,
+                private _router: Router,
+                private _userService: UserService) {
         this.form = fb.group({
             name: ['', Validators.required],
             email: ['', BasicValidators.email],
@@ -31,4 +35,11 @@ export class UserFormComponent implements CanDeactivate {
   
   		return true; 
   	}
+
+    save(){
+        this._userService.addUser(this.form.value)
+            .subscribe(x => {
+                this._router.navigate(['Users']);
+            })
+    }  
 }
