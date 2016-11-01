3b70c2e7011bf0e8fd10cd9de3cd636717cba04e
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-11-01 11:24:52 +0530

    deleting a user

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 import { UserService } from './users.service';
 import { RouterLink } from 'angular2/router';
 
@@ -7,17 +7,46 @@ import { RouterLink } from 'angular2/router';
     providers: [UserService],
     directives: [RouterLink]
 })
-export class UsersComponent { 
+export class UsersComponent {
     users: any[];
-    constructor(private _service: UserService){
+    constructor(private _service: UserService) {
 
     }
-    
-    ngOnInit(){
+
+    ngOnInit() {
         this._service.getUsers()
             .subscribe(users => this.users = users);
     }
 
- 
+    deleteUser(user){
+        if(confirm("Are you sure you want to delete " + user.name + "?")){
+            var index = this.users.indexOf(user);
+
+            this.users.splice(index, 1);
+            this._service.deleteUser(user.id)
+                .subscribe(null,
+                    err => {
+ 						alert("Could not delete the user.");
+                         // Revert the view back to its original state
+                         // by putting the user object at the index
+                         // it used to be.
+ 						this.users.splice(index, 0, user);
+ 					});
+        }
+    }
+
+ /*   deleteUser(user, e) {
+        if (confirm("You are about to delete the user " + user.name + ", are you sure?")) {
+            var  e = e || window.event;
+            e.path[2].hidden = true;
+            this._service.deleteUser(user.id)
+                .subscribe(null,
+                err => {
+                    alert("Could not delete user")
+                    event.path[2].hidden = false;
+                });
+        }*/
+    }
+
 
 }
--- app/users.service.ts
+++ app/users.service.ts
@@ -34,4 +34,9 @@ export class UserService {
         return this._url + "/" + userId;
     }
 
+    deleteUser(userId){
+        return this._http.delete(this.getUserUrl(userId))
+            .map(res => res.json());
+    }
+
 }
